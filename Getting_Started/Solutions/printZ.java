5cc83b20e88da26842032eeceecc3540eed3704b
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-05-24 01:13:14 +0530

    Day1_23/05/2022

---
+++ Getting_Started/Solutions/printZ.java
@@ -0,0 +1,15 @@
+package Getting_Started.Solutions;
+
+import java.util.*;
+
+public class printZ {
+
+	public static void main(String[] args) {
+		// Write your code here
+		System.out.println("*****");
+		System.out.println("   *");
+		System.out.println("  *");
+		System.out.println(" *");
+		System.out.println("*****");
+	}
+}
