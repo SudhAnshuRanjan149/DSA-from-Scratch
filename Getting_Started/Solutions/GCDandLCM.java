72a21c12bc31e875f737c855498619dcc7c778b0
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-06-01 22:24:19 +0530

    getting startred completed

--- Getting_Started/Solutions/GCDandLCM.java
+++ Getting_Started/Solutions/GCDandLCM.java
@@ -1,11 +1,6 @@
-public class GCDandLCM {
-	
-}
-
-// Not complete
 import java.util.*;
     
-    public class Main{
+    public class GCDandLCM{
     
     public static void main(String[] args) {
       // write your code here 
@@ -14,34 +9,26 @@ import java.util.*;
       int num1 = scn.nextInt();
       int num2 = scn.nextInt();
       
-    //   findGCD(num1,num2);
-      findLCM(num1,num2);
+      int GCD = findGCD(num1,num2);
+      System.out.println(GCD);
+      int LCM = (num1 * num2) / GCD;
+      System.out.println(LCM);
      }
      
-    //  public static void findGCD(num1,num2){
+     public static int findGCD(int num1,int num2){
+         int div = num1;
+         int max = num2;
+         if(num1 > num2){
+            max = num1;
+            div = num2;
+         }
          
-    //  }
-     public static void findLCM(int num1,int num2){
-        int div = 2;
-        int ans = 1;
-        
-        while(num1 != 0 && num2 != 0){
-            ans = ans * div;
-             int rem1 = num1 % div;
-             int rem2 = num2 % div;
-             
-             if(rem1 != 0 && rem2 != 0){
-                 div++;
-             }else if(rem1 == 0 && rem2 == 0){
-                 num1 = num1 / div;
-                 num2 = num2 / div;
-             }else if(rem1 == 0){
-                 num1 = num1 / div;
-             }else{
-                  num2 = num2 / div;
-             }
-        }
-        
-        System.out.println(ans);
+         int rem = max % div;
+         while(rem != 0){
+            max = div;
+            div = rem;
+            rem = max % div;
+         }
+         return div;
      }
     }
