775baa454666c22a1daa7e38f43df72e7f3954a1
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-07-10 00:14:50 +0530

    target sum subset by dp

--- Dynamic Programming and Greedy/Solutions/TargetSumSubsets_Dp.java
+++ Dynamic Programming and Greedy/Solutions/TargetSumSubsets_Dp.java
@@ -41,7 +41,51 @@
 // ************************************************************************************
 // Solution
 
+import java.io.*;
+import java.util.*;
 
 public class TargetSumSubsets_Dp {
-	
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn =  new Scanner(System.in);
+
+    int n = scn.nextInt();
+
+    int [] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+
+    int tar = scn.nextInt();
+
+    boolean [][] dp = new boolean[n + 1][tar + 1];
+
+    for (int i = 0; i < dp.length; i++) {
+      for (int j = 0; j < dp[0].length; j++) {
+        if (i == 0 && j == 0) {
+          dp[i][j] = true;
+        } else if (i == 0) {
+          dp[i][j] = false;
+        } else if (j == 0) {
+          dp[i][j] = true;
+        } else {
+          if (dp[i - 1][j] == true) {
+            dp[i][j] = true;
+          } else if (arr[i - 1] == j) {
+            dp[i][j] = true;
+          }
+          else if(arr[i - 1] < j && dp[i - 1][j - arr[i - 1]] == true) {
+            dp[i][j] = true;
+          }
+        else {
+            dp[i][j] = false;
+          }
+        }
+      }
+    }
+    
+    System.out.println(dp[dp.length -1][dp[0].length - 1]);
+
+  }
 }
